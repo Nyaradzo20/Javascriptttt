decd7ff4a17e87c12a178c7ac0c38c627c60c8b1
Nyaradzo Chinyati <nchinyati20@gmail.com>
2025-05-17 23:22:12 +0200

    Add square.js to generate a square pattern using nested loops

---
+++ practice/square.js
@@ -0,0 +1,12 @@
+for(let square = 1; square <=5; square++){
+    //outer loop for rows
+    line ='';
+    for(let t = 1; t <=4; t++){
+     line +='*';
+     //inner loop for columns
+     //console.log adds a new line, therefore we use line +=
+    }
+    console.log(line);
+
+     
+}
