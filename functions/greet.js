07a4206931c9e9137b6cdc0a11cced2b2e54791c
Nyaradzo Chinyati <nchinyati20@gmail.com>
2025-06-07 23:35:40 +0200

    Add string manipulation functions: getFirstChar, toUpperCase, and maxOfTwo

--- functions/greet.js
+++ functions/greet.js
@@ -17,3 +17,34 @@ let b = 7;
 
 let result = Add(a, b); 
 console.log(result);  
+
+//Write a function getFirstChar(str) that returns the first character of a string
+function getFirstChar(str){
+    return str[0];
+}
+
+let str = "FUMBLE";
+console.log(getFirstChar(str));
+
+//Write a function toUpperCase(str) that returns the uppercase version of the string.
+function toUpperCase(stri) {
+    return stri.toUpperCase();
+    
+}
+let stri = "not a loser!";
+console.log(toUpperCase(stri));
+
+//Write a function maxOfTwo(a, b) that returns the greater of two numbers.
+function maxOfTwo(c, d){
+    return Math.max(c,d);
+}
+
+let c = 5;
+let d =2;
+console.log(maxOfTwo(c,d));
+
+//Write a function reverseString(str) that returns the string reversed.
+
+function reverseString(strin){
+    
+}
