e9c68fa8d2e69b49121ebfaf63d5bdfa3458f6e1
Nyaradzo Chinyati <nchinyati20@gmail.com>
2025-06-11 22:49:00 +0200

    Add movie ranking and filtering functionality to arraysMovies.js

--- functions/arraysMovies.js
+++ functions/arraysMovies.js
@@ -6,6 +6,37 @@ let movies =[
   "John Wick"
 ]
 
+//iterate through loop
 for(const movie of movies){
     console.log(movie);
 }
+console.log()
+//rankk movies and push to new array
+console.log("rank movies and push to new array");
+let rankedMovies=[]
+let rank = 1
+for(const movie of movies){
+    rankedMovies.push(`${rank} ${movie}`)
+    rank++;
+}
+console.log(rankedMovies);
+console.log()
+console.log("FILTER MOVIES THAT START WITH")
+
+for(const filteredMovies of movies){
+    if( filteredMovies.startsWith("B"))
+    {
+        console.log(filteredMovies)
+        break;
+    }
+    else{
+        console.log("oops nothing to see here")
+    }
+}
+
+let movieCollection = [
+  { title: "Inception", director: "Christopher Nolan", year: 2010 },
+  { title: "Get Out", director: "Jordan Peele", year: 2017 },
+  { title: "The Matrix", director: "Wachowskis", year: 1999 },
+  { title: "Black Panther", director: "Ryan Coogler", year: 2018 }
+];
