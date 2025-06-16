5e864b0a2d5a4755c08653a5f91a79fc6dcc4e9e
Nyaradzo Chinyati <nchinyati20@gmail.com>
2025-06-16 14:55:26 +0200

    Refactor City and Drone classes; update addVillage method and constructor parameters for improved functionality

--- functions/PROJECTS/droneSimulator.js
+++ functions/PROJECTS/droneSimulator.js
@@ -5,6 +5,30 @@ class City{
         this.y = y;
         this.villages = villages;
     }
+
+    addVillage(village){
+        let existing = this.villages.find(vill=> vill.name === village.name)
+            if(existing){
+             console.log(`Village ${village.name} already exists in ${this.name}.`);
+    }
+    
+  else 
+  {
+        this.villages.push(village);
+        console.log(`Village ${village.name} added to ${this.name}.`);
+    }
+            }
+        
+    
+    
+    listVillages(city)
+    {
+    //  console.log(city.this.villages);
+      console.log(`Villages under ${this.name}:`);
+    for (let v of this.villages) {
+      console.log(`- ${v.name}`);
+    }
+    }
 }
 
 
@@ -52,16 +76,25 @@ class Medicine{
 }
 
 class Drone{
-    constructor(name, speed, x, y,batteryLife){
+    constructor(name, medicine, x, y,batteryLife, city, destination){
         this.name = name;
-        this.speed = speed;
         this.batteryLife = batteryLife;
+        this.medicine = medicine;
+        this.x = x;
+        this.y = y;
+        this.city = city;
+        this.destination = destination;
     }
 }
-const chivi = new Village("Chivi", 20, 15);
+/*const chivi = new Village("Chivi", 20, 15);
 
 chivi.deliveryLog(new Medicine("Panado", 10));
 chivi.deliveryLog(new Medicine("Malaria Meds", 50));
 chivi.deliveryLog(new Medicine("Panado", 10)); // Add more to Panado
 
-chivi.medList();
+chivi.medList();*/
+const harare = new City("Harare", 10, 20);
+const chivi = new Village("Chivi", 5, 8);
+const vil = new Village("Chivi", 5,8);
+harare.addVillage(chivi); // Adds Chivi to Harare
+harare.addVillage(vil);//already exists
